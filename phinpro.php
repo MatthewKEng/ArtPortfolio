ff2753490c706689e5714fa1d6e0fc976a92da09
Meng72 <matthewkeng@hotmail.com>
2017-02-13 21:22:09 -0600

    create php file

---
+++ phinpro.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+  </head>
+
+  <body>
+
+    <?php
+
+    /*
+    multiline
+    comment
+    */
+
+    //single line comment
+
+    # comment, as well
+
+    echo "<p>Phineas P Phantastically</>";
+
+
+
+    ?>
+
+
+  </body>
+
+
+</html>
