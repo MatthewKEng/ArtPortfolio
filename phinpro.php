b8b523deae93287c125036d72ab52e23530a945e
Meng72 <matthewkeng@hotmail.com>
2017-02-20 21:38:15 -0600

    add photo to php page

--- phinpro.php
+++ phinpro.php
@@ -27,8 +27,9 @@
     # comment, as well
 
     echo "<H1>Phineas' Funny Fables</H1></br>";
-    //<img src="https://lh3.googleusercontent.com/zKhRsIDCGjsHRv-qbUIPu20WaZNP6hDuqXwEqNx-bHSjMLCIsMlDbpJw8wdgL22ciypP1AxtEpa9iIHQi9mCOnHiVMNpBDurTFlBPlNGSPWPaTKAOsS7VyR8119mz-z8NpX4tZKyEs-WHWNtQZal09TiMwN7yabOGQjnnmBGHnN7FonnSG7-_ikk_Vs1Kf_GWGxG77ZWmz215Wghi5OJGIGonyzwO5Qkzw41l4ilQcCPvNYvxC5Y-BVYDr6IijJhm7LIYPmbDBixCdelb0HP1Lev9ikIgSnCibRjOMBckn_YciDowAo__VIQ_zsWpJMptlRcVP2CKp7q3k9n9MpF6f9ePHi8hVl7Z8TGpNkOIzkxksaHIkGpJ_rYD9l4VyKkRGWONfr1Y5_TzeONf1Rr_48cV1wiSiuqVfVGXt6H-swlsIj4zaU2_T3BAAUBkY_m_bhFDea-svH2hSCYB7Nvmb1vzfBG5Nni4sIvPUBYsePsbJQgJJyAtyKjc3LvgJpwfyGFE-ja-Fm21VDXncY0zv2mqLot9jtRs3E9AzIw8iYGet7KT2e74WKabvORltWKV62jbOvDNA6lF9bgI66_kKfZBPSkyya_afyruErTtVWAQD9-=w916-h873-no" alt="Phineas" style="width:auto;height:200px;display:block;margin:auto;">;
-
+    ?>
+    <img src="https://lh3.googleusercontent.com/zKhRsIDCGjsHRv-qbUIPu20WaZNP6hDuqXwEqNx-bHSjMLCIsMlDbpJw8wdgL22ciypP1AxtEpa9iIHQi9mCOnHiVMNpBDurTFlBPlNGSPWPaTKAOsS7VyR8119mz-z8NpX4tZKyEs-WHWNtQZal09TiMwN7yabOGQjnnmBGHnN7FonnSG7-_ikk_Vs1Kf_GWGxG77ZWmz215Wghi5OJGIGonyzwO5Qkzw41l4ilQcCPvNYvxC5Y-BVYDr6IijJhm7LIYPmbDBixCdelb0HP1Lev9ikIgSnCibRjOMBckn_YciDowAo__VIQ_zsWpJMptlRcVP2CKp7q3k9n9MpF6f9ePHi8hVl7Z8TGpNkOIzkxksaHIkGpJ_rYD9l4VyKkRGWONfr1Y5_TzeONf1Rr_48cV1wiSiuqVfVGXt6H-swlsIj4zaU2_T3BAAUBkY_m_bhFDea-svH2hSCYB7Nvmb1vzfBG5Nni4sIvPUBYsePsbJQgJJyAtyKjc3LvgJpwfyGFE-ja-Fm21VDXncY0zv2mqLot9jtRs3E9AzIw8iYGet7KT2e74WKabvORltWKV62jbOvDNA6lF9bgI66_kKfZBPSkyya_afyruErTtVWAQD9-=w916-h873-no" alt="Phineas" style="width:auto;height:200px;display:block;margin:auto;">;
+    <?php
     $noun = $_POST['noun'];
     $verb = $_POST['verb'];
     $adjective = $_POST['adjective'];
