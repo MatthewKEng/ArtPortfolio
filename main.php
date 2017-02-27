4eae48051a3696478c53470f5428d0cb08c9facd
Meng72 <matthewkeng@hotmail.com>
2017-02-26 21:24:58 -0600

    add main.php file

---
+++ main.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <style type="text/css">
+      h1,p {
+        text-align: center;
+      }
+      body {
+        background-color: #b3ffd9;
+      }
+      h1 {
+        font-size: 400%;
+        background-color: #b3ffd9;
+        font-family: Carton Six;
+      }
+      p {
+        font-size: 20px;
+        background-color: #b3ffd9;
+      }
+
+    </style>
+  </head>
+  <body>
+    <?php
+    echo "<H1>Phineas P. Phantastically</br>Productions</H1>"
+     ?>
+  </body>
+</html>
