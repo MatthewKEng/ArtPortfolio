b6d0d9e6a2788ef8f6ef8bc79ad9fc8a379c10b8
Meng72 <matthewkeng@hotmail.com>
2017-03-12 23:15:00 -0500

    move bio img to left

--- main.php
+++ main.php
@@ -10,9 +10,10 @@
       }
       body {
         background-color: #b3ffd9;
+      }
+      #first {
         display: flex;
         flex-direction: column;
-
       }
       #logo {
         align-items: center;
@@ -39,9 +40,14 @@
         opacity: 0.4;
       }
       #galleries {
-        position: relative;
-        text-align: center;
+        /*position: relative;*/
+        /*text-align: center;*/
+        display: flex;
+        flex-direction: column;
         padding-top: 600px;
+      }
+      #galleries H1 {
+        text-align: center;
       }
       p {
         font-size: 20px;
@@ -52,7 +58,11 @@
         justify-content: space-between;
       }
       .graphies {
-        
+        display: flex;
+        justify-content;
+        align-items: flex-start;
+        margin-top: 50px;
+
       }
       .graphies img {
         width: 175px;
@@ -62,20 +72,23 @@
     </style>
   </head>
   <body>
-    <div class="menu">
-      <p><a href="http://localhost/PhinPro/main.php#bios">BIOs</a></p>
-      <p><a href="http://localhost/PhinPro/main.php#galls">GALLERIES</a></p>
-      <p><a href="http://localhost/PhinPro/infoform.html">FUN!!!</a></p>
-    </div>
-    <div id="logo">
-      <H1>Phineas P. Phantastically</br>Productions</H1>
-      <img src="https://scontent-ord1-1.xx.fbcdn.net/v/t31.0-8/17039132_10154483283239716_5799823019574113614_o.jpg?oh=5ad9f00c7528b16553a90bc06c4d9684&oe=592A148A">
+    <div id="first">
+      <div class="menu">
+        <p><a href="http://localhost/PhinPro/main.php#bios">BIOs</a></p>
+        <p><a href="http://localhost/PhinPro/main.php#galls">GALLERIES</a></p>
+        <p><a href="http://localhost/PhinPro/infoform.html">FUN!!!</a></p>
+      </div>
+      <div id="logo">
+        <H1>Phineas P. Phantastically</br>Productions</H1>
+        <img src="https://scontent-ord1-1.xx.fbcdn.net/v/t31.0-8/17039132_10154483283239716_5799823019574113614_o.jpg?oh=5ad9f00c7528b16553a90bc06c4d9684&oe=592A148A">
+      </div>
     </div>
     <div id="galleries">
       <H1 id="bios">Biographies</H1>
       <div class="graphies">
         <img src="https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/17098678_10154485409229716_3093667700160597659_n.jpg?oh=5f15c1ce7892cb13d6d43567ac148515&oe=59635D03">
-        <p><H3>Matthew Eng</H3>Matthew has drawn ever since he could hold a crayon. Early drawings centered around recreations of action scenes from his favorite films, including Star Wars.</p>
+        <H3>Matthew Eng</H3>
+        <p>Matthew has drawn ever since he could hold a crayon. Early drawings centered around recreations of action scenes from his favorite films, including Star Wars.</p>
       </div>
       <div class="graphies">
         <img src="http://3.bp.blogspot.com/_4a3L1MNuWx8/R0pCuTMB4pI/AAAAAAAAAAU/BGbmYsEByow/S240/Phineas+P+Pooterbutt-Alpha.jpg">
